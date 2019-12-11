c3369bdee19707e7f943600a6ae4045b4e672b02
Sangbin Lee <sangbin1997@gmail.com>
2019-12-11 14:28:38 +0100

    [CLEANER] fix duplicated text

--- hivers/src/com/epita/tfidf/models/Cleaned.kt
+++ hivers/src/com/epita/tfidf/models/Cleaned.kt
@@ -1,3 +1,3 @@
 package com.epita.tfidf.models
 
-data class Cleaned(override val url: String, val text: String) : Document
+data class Cleaned(override val url: String, val title: String, val text: String) : Document
