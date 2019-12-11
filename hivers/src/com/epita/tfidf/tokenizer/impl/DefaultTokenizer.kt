7e0eeeb4460b25286d7e699b53c863cd6ffe9aad
Théo Issarni <issarnitheo@gmail.com>
2019-12-11 15:04:23 +0100

    [TOKENIZER] implementation is finished

--- hivers/src/com/epita/tfidf/tokenizer/impl/DefaultTokenizer.kt
+++ hivers/src/com/epita/tfidf/tokenizer/impl/DefaultTokenizer.kt
@@ -3,13 +3,15 @@ package com.epita.tfidf.tokenizer.impl
 import com.epita.tfidf.models.Cleaned
 import com.epita.tfidf.models.Tokenized
 import com.epita.tfidf.tokenizer.core.Tokenizer
+import com.epita.tfidf.utils.IO
+import com.epita.tfidf.utils.Stemming
 
 class DefaultTokenizer() : Tokenizer {
-    private val stopWords = listOf("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now").toList()
-    private val synonyms = hashMapOf("amazing" to listOf("incredible",  "unbelievable",  "improbable",  "fabulous",  "wonderful",  "fantastic",  "astonishing",  "astounding",  "extraordinary"),
-            "anger" to listOf("enrage",  "infuriate",  "arouse",  "nettle",  "exasperate",  "inflame",  "madden"),
-            "angry" to listOf("mad",  "furious",  "enraged",  "excited",  "wrathful",  "indignant",  "exasperated",  "aroused",  "inflamed"),
-            "answer" to listOf("reply",  "respond",  "retort",  "acknowledge"))
+
+    companion object {
+        private val stopWords = listOf("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now").toList()
+        private val synonyms = IO.synonymFromFile("../files/synonyms.txt")
+    }
 
     override fun compute(document: Cleaned): Tokenized {
         var tokens = slice(document.text)
@@ -20,18 +22,18 @@ class DefaultTokenizer() : Tokenizer {
     }
 
     private fun slice(text: String): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return text.split("[\\s,;\"'()!?._:+*]".toRegex())
     }
 
     private fun removeStopWord(tokens: List<String>): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return tokens.filter{ it !in stopWords }
     }
 
     private fun stemming(tokens: List<String>): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return tokens.map { Stemming.execute(it) }
     }
 
     private fun synonymReplacement(tokens: List<String>): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return tokens.map { if (synonyms.containsKey(it)) synonyms[it]!! else it }
     }
 }
